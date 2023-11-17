d3caa1f17913bf667da8b234886a523b2c5ec3c9
Douglas Ribeiro <dgrb0325@gmail.com>
2023-11-17 19:34:39 -0300

    feat: add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import cors from "cors";
 import { usersRoutes } from "./routes/users";
 import { ordersRoutes } from "./routes/orders";
 
 const app = express();
 
+const allowedOrigin = "http://localhost:3000";
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", usersRoutes);
 app.use("/orders", ordersRoutes);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 export { app };
